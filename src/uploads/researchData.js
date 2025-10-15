661f9dc61441f4404b10db16d26ea15459515aea
JihyeonOhk <jihyeon0674@gmail.com>
2025-10-15 10:51:52 +0900

    youtube&images

--- src/uploads/researchData.js
+++ src/uploads/researchData.js
@@ -28,20 +28,20 @@ const researchData = [
         desc: 'This study focuses on identifying suitable locations for highway-transfer Vertiports to integrate Urban Air Mobility (UAM) with existing highway infrastructure. UAM offers an effective solution for enhancing transportation accessibility in the Seoul Metropolitan Area, where conventional transportation often struggle to connect suburban employment zones such as industrial parks. By integrating UAM with ground transportation at highway facilities, an efficient connectivity solution can be achieved for regions with limited transportation options. Our proposed methodology for determining the suitable Vertiport locations utilizes data such as geographic information, origin-destination volume, and travel time. Vertiport candidates are evaluated and selected based on criteria including location desirability, combined transportation accessibility and transportation demand. Applying this methodology to the Seoul metropolitan area, we identify 56 suitable Vertiport locations out of 148 candidates. The proposed methodology offers a strategic approach for the selection of highway-transfer Vertiport locations, enhancing UAM integration with existing transportation systems. Our study provides valuable insights for urban planners and policymakers, with recommendations for future research to include real-time environmental data and to explore the impact of Mobility-as-a-Service on UAM operations. Detailed explanations are available on the following webpage.',
         hyperlink: 'https://scholar.google.com/citations?view_op=view_citation&hl=ko&user=W-lkXroAAAAJ&citation_for_view=W-lkXroAAAAJ:W7OEmFMy1HYC',
         youtube:['https://www.youtube.com/embed/uz03YgCI1MY?si=T9sQkTXhivEiuCxq'],
-        images: ['/researchImg/2-1.png'],
+        images: ['/researchImg/2-2.png'],
       },
       {
         title:'Data-driven future demand forecasting for UAM',
         date: '',
         desc: 'As the public interest in UAM is getting higher and the underlying technology for UAM becomes sophisticated, the need to forecast the future demand for UAM increases. As the data-driven demand forecasting method has become more accurate than the traditional modeling-based forecasting method, TRUE lab utilized Seoul taxi GPS data to forecast the potential demand for UAM. Our assumption is based on the idea that part of the long-range taxi trip will be replaced by the emerging mobility service - UAM. As our first step toward UAM demand forecasting, we considered the case of using existing helipads. We plan to expand our scope in future work to UAM routing, fleet rebalancing, and scheduling. For details, please visit our blog.',
-        images: ['/researchImg/2-2.png'],
+        images: ['/researchImg/2-3.png'],
       },
        {
         title:'UAM Concept of Operations',
         date: '',
         desc: `Urban Air Mobility (UAM) refers to an aviation transportation system that overcomes the limitations of existing terrestrial transportation methods and enables the transportation of people or cargo in three-dimensional urban airspace through vertical takeoff and landing (VTOL) vehicles, as well as the supporting infrastructure for takeoff and landing. UAM is expected to grow into a new advanced industry in future mobility, with a projected scale of over 700 trillion won over the next 20 years. Consequently, major mobility companies and governments around the world are rapidly joining the technological development efforts. In this context, in the first half of 2020, the Federal Aviation Administration (FAA) and the National Aeronautics and Space Administration (NASA) of the United States each announced specific concepts for UAM operations, known as UAM ConOps and UAM OpsCon Passenger-Carrying Operations, respectively. In line with this global trend, Korea also announced the 'Korean Urban Air Mobility (K-UAM) Roadmap,' including practical operational concepts tailored to the domestic context, as well as a technology development roadmap in the first half of 2020. Additionally, in June, we released the UAM operational concept document. Detailed explanations are available on the following webpage`,
         hyperlink: 'https://sites.google.com/view/urbanairmobility/home?authuser=1&pli=1',
-        images: ['/researchImg/2-2.png'],
+        images: ['/researchImg/2-4.png'],
       },
     ]
   },
@@ -56,7 +56,7 @@ const researchData = [
         title:'Regional for urban air mobility application in metropolitan areas: case studies in San Francisco and New York',
         date: '',
         desc: '​​In this study, as a first step to assess the feasibility of UAM in urban areas, we conduct 3D geodemographic analyses of two major cities in San Francisco, CA and Manhattan, NY. The 3D building footprint data is used to identify the raw available airspace as well as the added spatial restrictions with geofencing. Population data is used to represent the potential customer base by combining the daytime and nighttime population. Since the geospatial and demographic datasets differ in representation, the spatial data is vectorized while population data is available by census tract, spatial information is aggregated in census tracts. In addition, We proposed to group the areas of similar spatial and population characteristics through regionalization. Regionalization is a spatially constrained multi-variate clustering method to group small geographical units (census blocks and tracts in general) into a contiguous region of homogeneous nature. The main benefit of regionalization is to delineate regions of similar characteristics and spatial proximity. Through regionalization, one can better understand the urban space with comprehensive geographic perspective, rather than a small geographical unit of census blocks or tracts. Furthermore, regionalization can also improve geospatial intelligence in urban spaces by delineating the functional neighborhood . In this study, we adopted the SKATER, an efficient regionalization technique that uses minimum spanning tree consisting of a connected tree with no circuits. The intention is to provide a region map of the city that can readily identify regions of similar UAM operational and population characteristics with spatial continuity and feasibility. Based on the regionalization results, correspondence analysis was conducted to translate the compound effect of spatial and population characteristics into feasibility.',
-        images: ['/researchImg/5-1.png'],
+        images: ['/researchImg/5-2.png'],
       },
       {
         title:'Density-aware flight planning for multiple agents in urban airspace',
@@ -74,13 +74,13 @@ const researchData = [
 Namwoo Kim, Yoonjin Yoon, "Cooperative sUAV Collision Avoidance Based on Satisficing Theory", International Journal of Aeronautical and Space Sciences, 2019
         `,
         ref: ['Namwoo Kim, Yoonjin Yoon, "Cooperative sUAV Collision Avoidance Based on Satisficing Theory", International Journal of Aeronautical and Space Sciences, 2019'], 
-        images: ['/researchImg/5-2.png'],
+        images: ['/researchImg/5-3.png'],
       },
       {
         title:'Geodemographical Risk Analysis of 3D Urban Space',
         date: '',
         desc: `For application of UAS and UAM in urban area, people and man-mad structures that consisting urban environment should be considered. We analyzed urban space by using census tract based population data and 3d building model data. Highly urbanized areas – Manhattan Island and San Francisco were analyzed for their urban space characteristics.`,
-        images: ['/researchImg/5-3.png'],
+        images: ['/researchImg/5-4.png'],
       },
       {
         title:'Urban Airspace Availability Assessment',
@@ -95,7 +95,7 @@ This interactive tool enables you to explore the airspace availability in three 
 
 Jungwoo Cho, Yoonjin Yoon, "How to Assess the Capacity of Urban Airspace: A Topological Approach Using Keep-in and Keep-out Geofence", 92, 137-149, Transportation Research, Part C, 2018,  
 Method for identifying available airspace for unmanned aerial vehicle operations, Patent filed, Dec 17, 2017, KR 10-2018-0033978`,
-        images: ['/researchImg/5-4.png'],
+        images: ['/researchImg/5-5.png'],
       },
       {
         title:'Horizontal and Vertical Connectivity of Airspace ',
@@ -108,7 +108,7 @@ In this study, we present topography map and skeletal graph to interpret underly
 **SELECTED REFERENCES**
 
 Jungwoo Cho, Yoonjin Yoon. "Extraction and Interpretation of Geometrical and Topological Properties of Urban Airspace for UAS Operations". 13th ATM R&D Seminar, 2019`,
-        images: ['/researchImg/5-5.png'],
+        images: ['/researchImg/5-6.png'],
       },
       {
         title:'UTM: Urban Drone Routing',
@@ -135,7 +135,7 @@ Jungwoo Cho, Yoonjin Yoon. "Extraction and Interpretation of Geometrical and Top
 **REFERENCE**
 
 - Y. Shin, N. Kim, H. Lee, S.Y. In, M. Hansen, and Y. Yoon (2024), "Deep Learning Framework for Vessel Trajectory Prediction Using Auxiliary Tasks and Convolutional Networks". Engineering Applications of Artificial Intelligence, 132, 107936`,
-        images: ['/researchImg/3-1.png'],
+        images: ['/researchImg/3-1.png','/researchImg/3-2.png','/researchImg/3-3.png'],
       },
     ]
   },
