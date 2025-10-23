78269df4d921db9706ccce05e3daf0deccb1cd38
Jihyeon Ohk <jh.ohk@kaist.ac.kr>
2025-10-23 11:48:14 +0900

    conference_section_link

--- src/components/Publications/Conference.jsx
+++ src/components/Publications/Conference.jsx
@@ -39,21 +39,15 @@ const ConCard = ( item ) => (
 </div>
 )
 
-
-const SectionLink = () => (
-  // For floating link container
-  // <div className="fixed top-60 right-50 z-50 mt-8 flex flex-col flex-wrap gap-3 bg-gray-100 p-4 rounded-lg shadow-lg opacity-40 hover:opacity-100 transition">
-  <div className="mb-16 flex flex-wrap gap-3">
-      <Link to="/conference#international" className="inline-flex items-center px-5 py-3 bg-black text-white rounded-md text-sm font-semibold">International Conference</Link>
-      <Link to="/conference#national" className="inline-flex items-center px-5 py-3 border border-gray-300 rounded-md text-sm">National Conference Presentation</Link>
-  </div>
-)
-
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
       <h1 className="pagetitle">Conference Proceedings</h1>
     <section id="international" className="z-20 mb-16 mt-5">
-      <SectionLink />
+    {/* Section Links */}
+    <div className="mb-16 flex flex-wrap gap-3">
+      <Link to="/conference#international" className="inline-flex items-center px-5 py-3 bg-black text-white rounded-md text-sm font-semibold">International Conference</Link>
+      <Link to="/conference#national" className="inline-flex items-center px-5 py-3 border border-gray-300 rounded-md text-sm">National Conference Presentation</Link>
+    </div>
       <h2 className="sectiontitle text-2xl font-bold mb-4">International Conference</h2>
       <div className="space-y-6">
         {internationalData.map((item) => (
@@ -63,8 +57,11 @@ const SectionLink = () => (
       </section>
       
       <section id="national" className="mt-16">
-
-       <SectionLink />
+      {/* Section Links */}
+      <div className="mb-16 flex flex-wrap gap-3">
+        <Link to="/conference#international" className="inline-flex items-center px-5 py-3 border border-gray-300 rounded-md text-sm">International Conference</Link>
+        <Link to="/conference#national" className="inline-flex items-center px-5 py-3 bg-black text-white rounded-md text-sm font-semibold">National Conference Presentation</Link>
+      </div>
       <h2 className="sectiontitle text-2xl font-bold mb-4">National Conference Presentation</h2>
       <div className="space-y-6">
         {nationalData.map((item) => (
